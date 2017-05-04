736b0afcbcc8b1acf0c4fc8269221f28ff067605
Sean Fife <sfife@pepperdine.edu>
2017-05-04 10:13:40 -0700

    Update DedupeRESTPacket.cs

--- SfdcConnect/DataObjects/DedupeRESTPacket.cs
+++ SfdcConnect/DataObjects/DedupeRESTPacket.cs
@@ -1,4 +1,14 @@
-﻿using System;
+/****************************************************************************
+*
+*   File name: DataObjects\DedupeRESTPacket.cs
+*   Author: Sean Fife
+*   Create date: 5/2/2017
+*   Solution: SfdcConnect
+*   Project: SfdcConnect
+*   Description: Includes DedupeRESTPacket class for the custom Apex API endpoint
+*
+****************************************************************************/
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
